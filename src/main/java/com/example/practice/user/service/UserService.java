94ee8c05fb59ac8f83c2889457b839c828a52ad5
Joohee <ljh0667@gmail.com>
2023-08-10 20:21:17 +0900

    throw error

--- src/main/java/com/example/practice/user/service/UserService.java
+++ src/main/java/com/example/practice/user/service/UserService.java
@@ -3,7 +3,9 @@ package com.example.practice.user.service;
 import com.example.practice.user.User;
 import com.example.practice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 
@@ -22,17 +24,17 @@ public class UserService {
     }
 
     public User getUserById(Long id){
-        return userRepository.findById(id).get();
+        return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     public User updateUsername(Long id, String username){
-        User user = userRepository.findById(id).get();
+        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         user.setUsername(username);
         return userRepository.save(user);
     }
 
     public void deleteUser(Long id){
-        User user = userRepository.findById(id).get();
+        User user = userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));;
         userRepository.delete(user);
     }
 }
